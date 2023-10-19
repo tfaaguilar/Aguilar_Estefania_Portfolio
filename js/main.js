dbcd76c715a95fd19c211728abc224a96a150729
tfaaguilar@gmail.com <e_aguilarvelez@fanshaweonline.ca>
2023-10-18 22:09:47 -0400

    Burger menu

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 const logo = document.getElementById('logo');
 
     gsap.to(logo, {
-        duration: 2, 
-        x: 200, 
+        duration: 1, 
+        x: 20, 
         rotation: 360, 
         scale: 2, 
         backgroundColor: "#F2D8D8", 
@@ -11,23 +11,20 @@ const logo = document.getElementById('logo');
     });
 
 
+(function(){
 
+	let button = document.querySelector("#button");
+	let burgerCon = document.querySelector("#burger-con");
 
+	function hamburgerMenu() {
+		burgerCon.classList.toggle("slide-toggle");
+		button.classList.toggle("expanded");
+	};
 
+	// let hamburgerMenu = () => {
+	// 	burgerCon.classList.toggle("slide-toggle");
+	// 	button.classList.toggle("expanded");
+	// };
 
-
-
-
-
-
-
-const hamburgerCheckbox = document.getElementById("hamburger-checkbox");
-        const menu = document.querySelector(".menu");
-
-        hamburgerCheckbox.addEventListener("change", function () {
-            if (this.checked) {
-                menu.style.display = "block";
-            } else {
-                menu.style.display = "none";
-            }
-        });
+	button.addEventListener("click", hamburgerMenu, false);		
+})();
