02551177b073f5347abda44ef875503b6633c5f5
tfaaguilar <113370984+tfaaguilar@users.noreply.github.com>
2024-02-22 12:00:38 -0500

    Contact form
    Contact form updated including Data base

---
+++ adduser.php
@@ -0,0 +1,51 @@
+<?php
+
+    header("Access-Control-Allow-Origin: *");
+
+    header("Content-Type: application/json; charset=UTF-8");
+
+    
+    $db_host = 'localhost:8889';
+    $db_user = 'root';
+    $db_pass = 'root';
+    $db_name = 'a_e_portfolio';
+
+    
+    $connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+    $errors = array();
+
+    $fname = mysqli_real_escape_string($connection, $_POST['fname']);
+    if ($fname == NULL) {
+        $errors[] = "Name field is empty.";
+    }
+
+    $email = $_POST['email'];
+    if ($email == NULL) {
+        $errors[] = "Email field is empty.";
+    }
+
+    $message = mysqli_real_escape_string($connection, $_POST['message']);
+    if ($message == NULL) {
+        $errors[] = "Message field is empty.";
+    }
+
+    
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "\"" . $email . "\" is not a valid email address.";
+    }
+
+    $errcount = count($errors);
+    if ($errcount > 0) {
+        $errmsg = array();
+        for ($i = 0; $i < $errcount; $i++) {
+            $errmsg[] = $errors[$i];
+        }
+        echo json_encode(array("errors" => $errmsg));
+    } else {
+        $querystring = "INSERT INTO users(id,fname,email,message) VALUES(NULL,'" . $fname . "','" . $email . "','" . $message . "')";
+        $qpartner = mysqli_query($connection, $querystring);
+        echo json_encode(array("message" => "Form submitted. Thank you for your interest!"));
+    }
+    
+?>
