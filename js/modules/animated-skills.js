6475df29860de1807db84226ccac1017e7013df6
tfaaguilar <113370984+tfaaguilar@users.noreply.github.com>
2024-02-22 23:53:15 -0500

    Images
    Update profile

---
+++ js/modules/animated-skills.js
@@ -0,0 +1,15 @@
+
+// export function animateSkills() { 
+//     gsap.registerPlugin(SplitText)  
+
+//     const split = new SplitText('.skills', { type: 'chars' })
+
+//     const typing_text = gsap.timeline()
+//     .from(split.chars, {
+//     duration: .1,
+//     autoAlpha: 0,
+//     stagger: {
+//         each: .1
+//     }
+//     });
+// }
